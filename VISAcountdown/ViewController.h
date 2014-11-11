44f8bd589b546d2ae8646e4bc9b89b2b086383d1
hiratanaoki <usa.chinese220@gmail.com>
2014-11-11 13:19:30 +0800

    new initial commit

--- VISAcountdown/ViewController.h
+++ VISAcountdown/ViewController.h
@@ -8,7 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIPickerViewDataSource,UIPickerViewDelegate>{
+    
+    UILabel *_myLabelInput; //入力された文字を表示する
+    UILabel *_countryLabel; //選択された国名を表示する
+    UILabel *_purposeLabel; //選択された目的を表示する
+    UILabel *_periodLabel; //選択された期間を表示する
+    
+    
+    UIView *_backView;  //水色のビュー用メンバ変数
+    BOOL *_visibleFlag;  //水色のビューが表示されている:YES 非表示:NO
+    
+    UIButton *_CheckButton;    //金額チェックボタンを作成する
+    UIButton *_CancelButton;    //キャンセルボタンを作成する
+    UIButton *_finishButton;    //登録完了ボタンを作成する
+    UIButton *_countryButton;   //国名を選ぶボタン
+    UIButton *_purposeButton;   //目的を選ぶボタン
+    UIButton *_periodButton;    //期間を選ぶボタン
+    UIButton *_createButton;    //addSubView内の完了ボタン
+    UIButton *_setButton;       //設定を完了するボタン
+    
+    UIPickerView *_PickerView;  //選択項目を表示
+    
+    NSArray *_country;  //国名の連想配列
+    
+}
+
 
 
 @end
--- VISAcountdown/ViewController.m
+++ VISAcountdown/ViewController.m
@@ -18,7 +18,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)textLabel{
+    _myLabelInput = [[UILabel alloc] initWithFrame:CGRectMake(20 ,80 ,200, 50)];
+    
+    _myLabelInput.text = @"情報を入力";
+    [self.view addSubview:_myLabelInput];
+}
+-(void)countryLabel{
+    _countryLabel = [[UILabel alloc] initWithFrame:CGRectMake(20 ,160 ,200, 50)];
+    
+    _countryLabel.text = @"";
+    [self.view addSubview:_countryLabel];
+}
+-(void)periodLabel{
+    _periodLabel = [[UILabel alloc] initWithFrame:CGRectMake(20 ,220 ,200, 50)];
+    
+    _periodLabel.text = @"";
+    [self.view addSubview:_periodLabel];
+}
+-(void)purposeLabel{
+    _purposeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20 ,280 ,200, 50)];
+    
+    _purposeLabel.text = @"";
+    [self.view addSubview:_purposeLabel];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
